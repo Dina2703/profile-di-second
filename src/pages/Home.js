d0f1810d09824e9ed8152acb148beb1ef545f393
Dinara Idrissova <Dinara.Idrissova@list.ru>
2023-01-08 17:47:36 -0500

    Home: action buttons text alignment

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,8 +5,8 @@ import { Link } from "react-router-dom";
 function Home() {
   return (
     <div className="flex flex-col gap-y-2 md:gap-y-12 ">
-      <div className="flex flex-col-reverse items-center md:flex-row w-[80vw] overflow-hidden justify-center  md:gap-10">
-        <div className=" flex flex-col  justify-end gap-y-1 font-semibold text-gray-600 text-lg md:text-3xl  p-3 md:p-6 ">
+      <div className="flex  flex-col-reverse items-center md:flex-row w-[80vw] overflow-hidden justify-center  md:gap-10">
+        <div className=" flex flex-col justify-end gap-y-1 font-semibold text-gray-600 text-lg md:text-1xl lg:text-3xl p-3 md:p-6 ">
           <p className="text-gray-400 text-xs md:text-sm italic mb-1 md:mb-3">{`<div>`}</p>
           <pre className="">
             <p>Hello!</p>
@@ -25,19 +25,21 @@ function Home() {
           <img
             src={img}
             alt="Dinara Idrissova"
-            className=" w-[200px] h-[200px] rounded-md  md:w-[300px] md:h-[300px]  object-cover md:rounded-full object-top shadow-md shadow-neutral-400 "
+            className=" w-[200px] h-[200px] rounded-md md:w-[250px] md:h-[250px] lg:w-[300px] lg:h-[300px]  object-cover md:rounded-full object-top shadow-md shadow-neutral-400  "
           />
         </div>
       </div>
       <div className="flex md:flex-col flex-row justify-center items-center gap-2  p-2">
         <Link to={"/projects"}>
           <button class="bg-orange-500 hover:bg-orange-600 text-white font-bold py-2 px-4 rounded-full text-sm md:text-lg w-40 h-10 md:h-12 md:w-60 flex justify-center items-center gap-2 shadow-md shadow-gray-400">
-            <pre>my portfolio</pre> <FaChevronRight />
+            <pre className="w-36 flex justify-start">my portfolio</pre>{" "}
+            <FaChevronRight />
           </button>
         </Link>
         <Link to={"/"}>
           <button class="bg-orange-500 hover:bg-orange-600 text-white font-bold py-2 px-4 rounded-full text-sm md:text-lg w-40 md:w-60 h-10 md:h-12 flex justify-center items-center gap-2 shadow-md shadow-gray-400">
-            <pre>my resume</pre> <FaChevronRight />
+            <pre className="w-36 flex justify-start"> my resume</pre>{" "}
+            <FaChevronRight />
           </button>
         </Link>
       </div>
