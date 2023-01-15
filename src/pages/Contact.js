9f6959b5dcc6321303bffd3cc7ef9f0ab8b6d9f3
Dinara Idrissova <Dinara.Idrissova@list.ru>
2023-01-14 23:27:31 -0500

    Contact page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,9 +1,45 @@
-function Contact() {
+import React from "react";
+import { FaRegEnvelope } from "react-icons/fa";
+import { BsTelephone } from "react-icons/bs";
+import { MdOutlineLocationOn } from "react-icons/md";
+
+const Contact = () => {
+  const handleClick = () => {
+    window.open("https://www.google.com/maps?q=alpharetta, GA", "_blank");
+  };
   return (
-    <div className="h-screen">
-      <h1></h1>
+    <div className="overflow-x-hidden py-4 max-w-3xl px-4 2xl:mt-16  ">
+      <div className="flex flex-col gap-3 justify-center items-start">
+        <h2 className="font-semibold">Get in Touch</h2>
+        <p className="flex gap-1 items-center hover:underline">
+          <FaRegEnvelope color="rgb(249 115 22)" />:{" "}
+          <a href="mailto:dina.b.idrissova@gmail.com">
+            dina.b.idrissova@gmail.com
+          </a>
+        </p>
+        <p className="flex gap-1 items-center hover:underline ">
+          <BsTelephone color="rgb(249 115 22)" className="font-bold" />:{" "}
+          <a href="tel:+14042457101">(404) 245-7101</a>
+        </p>
+        <p className="flex gap-1 items-center hover:underline cursor-pointer">
+          <MdOutlineLocationOn
+            onClick={handleClick}
+            color="rgb(249 115 22)"
+            className="font-bold text-lg"
+          />
+          : Atlanta, Georgia, USA
+        </p>
+        <p>
+          <span className="font-semibold">
+            Thank you for visiting my website!{" "}
+          </span>{" "}
+          <br />
+          If you have any questions, comments or just want to say hi, don't
+          hesitate to reach out. <br /> I'd love to hear from you!
+        </p>
+      </div>
     </div>
   );
-}
+};
 
 export default Contact;
